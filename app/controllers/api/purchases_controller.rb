9e5682e0e361740af2469bd21c8d9c57bc1fa5b3
alectroemel <alectroemel@hotmail.com>
2016-04-26 17:21:49 -0500

    changed to using rmagick

--- app/controllers/api/purchases_controller.rb
+++ app/controllers/api/purchases_controller.rb
@@ -54,6 +54,7 @@ class Api::PurchasesController < ApplicationController
 	def updateImage(listOfPixelsIDs)
 		require 'aws-sdk'
 		require "rmagick"
+		require "open-uri"
 
 		s3 = Aws::S3::Resource.new(:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
                           :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
@@ -62,16 +63,12 @@ class Api::PurchasesController < ApplicationController
 		bucket = s3.bucket(ENV['S3_BUCKET_NAME'])
 
 		# cache the current image to the server to process it 
-		local_current  = "app/assets/images/tmp/current.png"
-		s3current = bucket.object('current.png')
-	    File.open(local_current, 'wb') do |file|
-			photo.read do |chunk|
-	        	file.write(chunk)
-      		end        
-	    end
+		File.open('app/assets/images/current.png', 'wb') do |fo|
+		  fo.write open("https://s3-us-west-2.amazonaws.com/fmsc4262016/current.png").read 
+		end
 
 	    # copy the pixels 
-		current = Magick::Image.read("app/assets/images/tmp/current.png").first
+		current = Magick::Image.read("app/assets/images/current.png").first
 		master = Magick::Image.read("app/assets/images/master.png").first
 
 		for pixel in listOfPixelsIDs
@@ -85,7 +82,7 @@ class Api::PurchasesController < ApplicationController
 		end 
 
 		# write back to the aws bucket 
-		current.write("app/assets/images/tmp/current.png")
-		bucket.object('test.png').upload_file('app/assets/images/tmp/current.png')
+		current.write("app/assets/images/current.png")
+		bucket.object('test.png').upload_file('app/assets/images/current.png')
 	end 
 end
