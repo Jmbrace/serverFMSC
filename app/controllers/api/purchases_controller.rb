e718173c0ccf79e6b0b206a5d5e2ad9d9dccc511
alectroemel <alectroemel@hotmail.com>
2016-04-26 16:50:43 -0500

    changed to using rmagick

--- app/controllers/api/purchases_controller.rb
+++ app/controllers/api/purchases_controller.rb
@@ -66,7 +66,7 @@ class Api::PurchasesController < ApplicationController
 		# obj.upload_file("app/assets/images/current.png")
 
 		s3 = Aws::S3::Resource.new(:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-                          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+                          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
                           :region => 'us-west-2')
 		s3.bucket(ENV['S3_BUCKET_NAME']).object('test.png').upload_file('app/assets/images/current.png')
 
