7110e52eaf395b6d12d5c468356fbf887abf83e7
alectroemel <alectroemel@hotmail.com>
2016-04-26 16:25:27 -0500

    changed to using rmagick

--- app/controllers/api/purchases_controller.rb
+++ app/controllers/api/purchases_controller.rb
@@ -63,7 +63,7 @@ class Api::PurchasesController < ApplicationController
 
 		s3 = Aws::S3::Resource.new(region:'us-west-2')
 		obj = s3.bucket(ENV['S3_BUCKET_NAME']).object(key: ENV['AWS_SECRET_ACCESS_KEY'])
-		# obj.upload_file('app/assets/images/current.png')
+		obj.upload_file("app/assets/images/current.png")
 
 
 		# currentImage = ChunkyPNG::Image.from_file('app/assets/images/current.png')
