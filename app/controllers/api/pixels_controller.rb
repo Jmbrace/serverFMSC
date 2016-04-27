09886a79b1e807241f3628996d6369feecc69935
Jmbrace <jmbrace@ilstu.edu>
2016-04-27 11:42:08 -0500

    pixels...

--- app/controllers/api/pixels_controller.rb
+++ app/controllers/api/pixels_controller.rb
@@ -4,21 +4,26 @@ class Api::PixelsController < ApplicationController
 
 
 	# batch,batch_size = [], 1_000_000
-	  	for xPos in 1..500
-	  		for yPos in 1..500
-	  			pixel = Pixel.new()
-	  			pixel.x = xPos
-	  			pixel.y = yPos
-	  			# batch << pixlel
-	  			pixel.save
-	  		end
-	  	end
+
+		# Thread.new do
+		#  for xPos in 1..1000
+	 #  		for yPos in 1..1000
+	 #  			pixel = Pixel.new()
+	 #  			pixel.x = xPos
+	 #  			pixel.y = yPos
+	 #  			# batch << pixlel
+	 #  			pixel.save
+	 #  		end
+	 #  	end
+		#   end
+		#   ActiveRecord::Base.connection.close
+		# end
 	  # if batch.size >= batch_size
 	  #   Product.import batch
 	  #   batch = []
 	  # end
 	# Product.import batch
-	  	# Pixel.delete_all
-	  	render json: { :message => Pixel.count }
+	  	Pixel.delete_all
+	  	render json: { :message => "we cool" }
 	  end
 end
