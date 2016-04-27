a4489541f9159033950a094cb7a99944905679ae
Jmbrace <jmbrace@ilstu.edu>
2016-04-27 11:36:04 -0500

    Splitting up pixel creation

--- app/controllers/api/pixels_controller.rb
+++ app/controllers/api/pixels_controller.rb
@@ -4,21 +4,21 @@ class Api::PixelsController < ApplicationController
 
 
 	# batch,batch_size = [], 1_000_000
-	  	# for xPos in 1..1000
-	  	# 	for yPos in 1..1000
-	  	# 		pixel = Pixel.new()
-	  	# 		pixel.x = xPos
-	  	# 		pixel.y = yPos
-	  	# 		# batch << pixlel
-	  	# 		pixel.save
-	  	# 	end
-	  	# end
+	  	for xPos in 1..500
+	  		for yPos in 1..500
+	  			pixel = Pixel.new()
+	  			pixel.x = xPos
+	  			pixel.y = yPos
+	  			# batch << pixlel
+	  			pixel.save
+	  		end
+	  	end
 	  # if batch.size >= batch_size
 	  #   Product.import batch
 	  #   batch = []
 	  # end
 	# Product.import batch
-	  	Pixel.delete_all
+	  	# Pixel.delete_all
 	  	render json: { :message => Pixel.count }
 	  end
 end
