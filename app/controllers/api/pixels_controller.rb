38c7839049647971a5888ad13bc093146bac9e42
Jmbrace <jmbrace@ilstu.edu>
2016-04-26 23:42:05 -0500

    fixing pixels

--- app/controllers/api/pixels_controller.rb
+++ app/controllers/api/pixels_controller.rb
@@ -1,12 +1,14 @@
 class Api::PixelsController < ApplicationController
   def show
-  	for xPos in 1..1000000
-  		for yPos in 1..1000000
-  			pixel = Pixel.new()
-  			pixel.x = xPos
-  			pixel.y = yPos
-  			pixel.save
-  		end
-  	end
+  	# for xPos in 1..1000000
+  	# 	for yPos in 1..1000000
+  	# 		pixel = Pixel.new()
+  	# 		pixel.x = xPos
+  	# 		pixel.y = yPos
+  	# 		pixel.save
+  	# 	end
+  	# end
+
+  	Pixel.delete_all
   end
 end
