2ae9bbb5f6070d491dda2bd0f224accdb2fc7b98
Jmbrace <jmbrace@ilstu.edu>
2016-04-26 23:35:17 -0500

    Init pixels

--- app/controllers/api/pixels_controller.rb
+++ app/controllers/api/pixels_controller.rb
@@ -1,4 +1,12 @@
 class Api::PixelsController < ApplicationController
   def show
+  	for xPos in 1..1000000
+  		for yPos in 1..1000000
+  			pixel = Pixel.new()
+  			pixel.x = xPos
+  			pixel.y = yPos
+  			pixel.save
+  		end
+  	end
   end
 end
