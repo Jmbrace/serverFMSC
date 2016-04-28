1350f4004bc779962c10647338d1d5129d69f19c
Jmbrace <jmbrace@ilstu.edu>
2016-04-28 03:11:56 -0500

    test

--- app/controllers/api/payments_controller.rb
+++ app/controllers/api/payments_controller.rb
@@ -23,8 +23,8 @@ class Api::PaymentsController < ApplicationController
     counter = 0
     while counter < numOfBlocks
       block = Block.where(status: "")
-      block.status = "purchased"
-      block.message = message
+      # block.status = "purchased"
+      # block.message = message
       # block.save
       listOfBlocks.push(block)
       counter = counter + 1
