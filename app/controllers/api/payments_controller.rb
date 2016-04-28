e172632bc3fdd9030ebe30bea01b8fb1468a05f4
alectroemel <alectroemel@hotmail.com>
2016-04-28 13:20:27 -0500

    working on purchase

--- app/controllers/api/payments_controller.rb
+++ app/controllers/api/payments_controller.rb
@@ -21,21 +21,21 @@ class Api::PaymentsController < ApplicationController
     numOfBlocks = numOfMeals / 20
     listOfBlocks = Array.new
 
-		Block.where(status: "").first(numOfBlocks).find_each do |block|
-  		block.status = "purchased"
-  		block.message = message
-  		block.state = state
-  		block.save
-  		listOfBlocks << block
-		end
+		# Block.where(status: "").first(numOfBlocks).find_each do |block|
+  # 		block.status = "purchased"
+  # 		block.message = message
+  # 		block.state = state
+  # 		block.save
+  # 		listOfBlocks << block
+		# end
 
-    result = Braintree::Transaction.sale(
-    :amount => params[:amount], #"10.00", #could be any other arbitrary amount captured in params[:amount] if they weren't all $10.
-    :payment_method_nonce => nonce,
-    :options => {
-      :submit_for_settlement => true
-      }
-    )
+  #   result = Braintree::Transaction.sale(
+  #   :amount => params[:amount], #"10.00", #could be any other arbitrary amount captured in params[:amount] if they weren't all $10.
+  #   :payment_method_nonce => nonce,
+  #   :options => {
+  #     :submit_for_settlement => true
+  #     }
+  #   )
 
     #updateImage(listOfBlocks)
     render json: { message: "We did it" }
