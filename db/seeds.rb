56296a40e7442bd22eef495e6f590509b0d5cbe5
Jmbrace <jmbrace@ilstu.edu>
2016-04-27 23:01:49 -0500

    no message

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-  	for blockX in 1..250 
-  		for blockY in 51..200
+  	for blockX in 1..70 
+  		for blockY in 1..200
   		Block.create!({leftXCrnr: (blockX-1) * 16, leftYCrnr: (blockY-1) * 15, status: ""})
   		end
   	end
