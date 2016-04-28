32e80b814ca62c668d5bbc0ed0d5af5a33731a6d
Jmbrace <jmbrace@ilstu.edu>
2016-04-28 00:04:39 -0500

    blocks

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-  	for blockX in 161..200
-  		for blockY in 1..200
-  		Block.create!({leftXCrnr: (blockX-1) * 16, leftYCrnr: (blockY-1) * 15, status: ""})
-  		end
+  	# for blockX in 161..200
+  	# 	for blockY in 1..200
+  	# 	Block.create!({leftXCrnr: (blockX-1) * 16, leftYCrnr: (blockY-1) * 15, status: ""})
+  	# 	end
+  	# end
+
+  	for blockX in 121..160
+  		Block.where(leftXCrnr: (blockX-1) * 16).delete_all
   	end
 
   puts "Created #{Block.count} Blocks"
