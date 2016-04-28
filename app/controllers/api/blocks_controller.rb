93aec13a85c996424f22482085495e328b2b391a
Jmbrace <jmbrace@ilstu.edu>
2016-04-27 20:57:42 -0500

    blocks

--- app/controllers/api/blocks_controller.rb
+++ app/controllers/api/blocks_controller.rb
@@ -1,15 +1,15 @@
 class Api::BlocksController < ApplicationController
   
   def show
-  	for blockX in 1..250 
-  		for blockY in 1..100
-  		  block = Block.new()
-  		  block.leftXCrnr = (blockX-1) * 16
-  		  block.leftYCrnr = (blockY-1) * 15
-  		  block.status = ""
-  		  block.save
-  		end
-  	end
+  	# for blockX in 1..250 
+  	# 	for blockY in 1..100
+  	# 	  block = Block.new()
+  	# 	  block.leftXCrnr = (blockX-1) * 16
+  	# 	  block.leftYCrnr = (blockY-1) * 15
+  	# 	  block.status = ""
+  	# 	  block.save
+  	# 	end
+  	# end
 
   	# for blockX in 1..250 
   	# 	for blockY in 101..200
@@ -19,7 +19,7 @@ class Api::BlocksController < ApplicationController
   	# 	  block.status = ""
   	# 	end
   	# end
-
+  	Block.delete_all
   	render json: {message: Block.count } 
   end
 
