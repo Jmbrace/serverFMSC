3051a43e56d155db95e4d8d76da40a0690529322
alectroemel <alectroemel@hotmail.com>
2016-04-28 12:24:16 -0500

    lets get payments working!

--- app/controllers/api/blocks_controller.rb
+++ app/controllers/api/blocks_controller.rb
@@ -1,7 +1,7 @@
 class Api::BlocksController < ApplicationController
   
   def show
-    render json: Block.where(status: "").first(10)
+    render json: Block.first(10) #Block.where(status: "").first(10)
   end
 end
 
